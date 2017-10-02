26b86cc982972af0a4b4b0093290cc6e36189cf0
Santhosh Mohan <santhoshm309@gmail.com>
2017-10-02 14:25:50 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+
+import {
+  AfterViewInit, Component, ElementRef, ViewChild, trigger,
+  state,
+  style,
+  transition,
+  animate,
+  keyframes, OnInit
+} from '@angular/core';
+import {TweenMax} from 'gsap';
+import {TimelineMax} from 'gsap';
+import {Linear} from 'gsap';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('rotateSun', [
+      transition('*<=>*', [
+        style({
+          transform: 'rotate(180deg)',
+        })
+        ,animate('980ms')] ),
+    ]),
+
+
+  ]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit{
   title = 'app';
+  shaObj : any;
+  hash : String;
+  astrounaut1 : any;
+  @ViewChild('sun') sun : ElementRef;
+  anim : any;
+  state: boolean = true;
+
+  ngAfterViewInit(){
+    setInterval(() => {
+      this.state = !this.state
+    }, 1000);
+  }
+
+  ngOnInit(){
+
+  }
+
+  constructor( ){
+
+  }
+
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,8 @@
-/* SystemJS module definition */
+ /* SystemJS module definition */
 declare var module: NodeModule;
+declare module 'sha';
+
+declare module 'gsap'
 interface NodeModule {
   id: string;
 }
